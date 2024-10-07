9105ec91d44de6ec6c745de2af454b447a447a4a
RilzSnep <fff.fff.fff.08.fff@mail.ru>
2024-10-07 12:36:05 +0300

    hw "for" try 2

--- src/Main.java
+++ src/Main.java
@@ -1,47 +1,58 @@
 public class Main {
     public static void main(String[] args) {
-        for (int i = 0; i <= 10; i++)
+        for (int i = 0; i <= 10; i++) {
             System.out.println(i);
+        }
         System.out.println(" ");
 
 
-        for (int i = 10; i >= 0; i -= 1)
+        for (int i = 10; i >= 0; i -= 1) {
             System.out.println(i);
+        }
+        System.out.println(" ");
+
+        for (int i = 2; i < 17; i += 2) {
+            System.out.println(i);
+        }
+        System.out.println(" ");
+
+        for (int i = 10; i >= -10; i -= 1) {
+            System.out.println(i);
+        }
         System.out.println(" ");
 
 
-        for (int i = 2; i < 17; i += 2)
-            System.out.println(i);
-        System.out.println(" ");
-
-        for (int i = 10; i >= -10; i -= 1)
-            System.out.println(i);
-        System.out.println(" ");
-
-
-        for (int i = 1904; i <= 2096; i += 4)
+        for (int i = 1904; i <= 2096; i += 4) {
             System.out.println(i + " год является високосным");
+        }
         System.out.println(" ");
 
 
-        for (int i = 7; i <= 100; i += 7)
+        for (int i = 7; i <= 100; i += 7) {
             System.out.println(i);
+        }
         System.out.println(" ");
 
 
-        for (int i = 1; i <= 600; i *= 2)
+        for (int i = 1; i <= 600; i *= 2) {
             System.out.println(i);
+        }
         System.out.println(" ");
 
+        int totalSavings = 0;
+        int monthlySavings = 29000;
+        double interestRate = 0.12;
 
-        int mounthCount = 12;
-        for (int i = 1; i <= mounthCount; i++)
-            System.out.println("Месяц " + i + " Сумма накоплений равна " + (i * 29000 * 1.01) + " рублей");
-        System.out.println(" ");
+        for (int month = 1; month <= 12; month++) {
+            totalSavings += monthlySavings;
+            totalSavings *= (1 + interestRate / 12);
+            System.out.println("Месяц " + month + ", сумма накоплений равна " + totalSavings + " рублей.");
+        }
 
 
-        for (int i = 1; i <= 10; i++)
+        for (int i = 1; i <= 10; i++) {
             System.out.println("2*" + i + "=" + 2 * i);
+        }
         System.out.println(" ");
     }
 }
